ef353ac4e24170cfbee9441d9ff04ecfa90accd5
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-07-11 16:32:19 +0000

    fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+--Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+SELCET origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDERED BY metal_bands
