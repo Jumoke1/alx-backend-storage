5de70ffa68ce223a3a5e80d76b5d5a432c062aee
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-07-17 22:47:22 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,15 +8,15 @@ from typing import Union
 
 class Cache:
     '''defines a class cache'''
+
     def __init__(self):
         '''Initialize the Cache with a Redis client and flush the database'''
-    self._redis = redis.Redis()
-    self._redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-
-def store(self, data: Union[str, bytes, int, float]) -> str:
-    '''defines a method that takes one
-    argument data which can be of any type'''
-    key = str(uuid.uuid4())
-    self._redis.set(key, data)
-    return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''defines a method that takes one
+        argument data which can be of any type'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
