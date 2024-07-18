f3ec40fd0c5298ed869bf66e06d25dc7b28219b4
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-07-18 04:01:25 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,3 +20,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable
+            [[bytes], object]] = None) -> Optional[object]:
+        """Retrieve data from Redis using the given key,
+        optionally applying fn for conversion."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Convenience method to retrieve data as UTF-8 decoded string."""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Convenience method to retrieve data as integer."""
+        return self.get(key, int)
